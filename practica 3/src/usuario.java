b551174fc31e02caf4ef1fa913228be72e9f7f3a
normahtds <125906525+normahtds@users.noreply.github.com>
2025-01-27 15:11:20 -0600

    ejercicio 1 se realizo bien

---
+++ practica 3/src/usuario.java
@@ -0,0 +1,14 @@
+
+/*import java.util.Scanner;
+public class usuario {
+    public static void main(String[] args) {
+
+        System.out.println("Ingresa tu nombre: ");
+        String dato=scn.nextLine();
+        System.out.println("Ingresa tu apellido paterno: ");
+        System.out.println("Ingrese su apellido materno: ");
+
+        String usuario=
+        )
+    }
+}*/
