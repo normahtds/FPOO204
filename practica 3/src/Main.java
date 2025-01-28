6018c21e654e438cd2573cdca5148c089b164684
normahtds <125906525+normahtds@users.noreply.github.com>
2025-01-27 21:54:40 -0600

    se realizo por completo el ejercicio 2 nombre del usuario

---
+++ practica 3/src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+        Scanner total=new Scanner(System.in);
+
+        System.out.println("¿Cual es tu salario?");
+        int salario=total.nextInt();
+
+
+        System.out.println("¿Cuantas horas trabajo?");
+        int horas=total.nextInt();
+
+        System.out.println("El salario total es: " + (salario * horas));
+
+    }
+}*/
--- practica 3/src/usuario.java
+++ practica 3/src/usuario.java
@@ -1,14 +1,40 @@
 
-/*import java.util.Scanner;
+import java.util.Scanner;
+
 public class usuario {
     public static void main(String[] args) {
 
-        System.out.println("Ingresa tu nombre: ");
-        String dato=scn.nextLine();
-        System.out.println("Ingresa tu apellido paterno: ");
-        System.out.println("Ingrese su apellido materno: ");
+        Scanner usuario = new Scanner(System.in);
 
-        String usuario=
-        )
+        System.out.println("Ingresa tu nombre: ");
+        String nombre = usuario.nextLine();
+        System.out.println("Ingresa tu apellido paterno: ");
+        String apellidop = usuario.nextLine();
+        System.out.println("Ingrese su apellido materno: ");
+        String apellidom = usuario.nextLine();
+
+        String usuariocom = (nombre + " " + apellidop + " " + apellidom);
+
+        System.out.println(usuariocom); //nombre solo
+
+        System.out.println("Nombre de usuario completo tres veces: ");  // nombre repetido tres veces
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Repetir nombre" + i + ": " + usuariocom);
+        }
+
+        System.out.println("Nombre de usuarios en minusculas: " + usuariocom.toLowerCase()); //nombre con minusculas
+
+        System.out.println("Nombre de usuarios en mayusculas: " + usuariocom.toUpperCase()); //nombre con mayusculas
+
+        String[] separ = usuariocom.split(" ");
+        StringBuilder user = new StringBuilder();
+
+        for (String usseer : separ) {
+            if (!usseer.isEmpty()) {
+                user.append(Character.toUpperCase(usseer.charAt(0)));
+
+            }
+        }
+        System.out.println(user.toString());
     }
-}*/
+}
