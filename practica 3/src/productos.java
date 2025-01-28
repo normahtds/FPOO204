cb143b64d0e316a8b041951dcb48d31ae06d76de
normahtds <125906525+normahtds@users.noreply.github.com>
2025-01-27 23:45:34 -0600

    se realizo el ejercicio 5 sobre el peso de los payasos y muñecas

---
+++ practica 3/src/productos.java
@@ -0,0 +1,24 @@
+/*
+import java.util.Scanner;
+
+public class productos {
+
+    public static void main(String[] args) {
+
+        Scanner producto = new Scanner(System.in);
+
+        System.out.println("Ingrese el numero de los payasos vendidos: ");
+        int payaso = producto.nextInt();
+
+        System.out.println("Ingrese el numero de las muñecas vendidas: ");
+        int muneca = producto.nextInt();
+
+        int pespayaso = 112;
+        int pesmuneca =75;
+
+        int total = (payaso * pespayaso + muneca * pesmuneca);
+
+        System.out.println("El peso total de los productos " + total + "gramos");
+    }
+}
+*/
