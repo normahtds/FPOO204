736e5da9220e12b1e06e280f8cf803361755d82f
normahtds <125906525+normahtds@users.noreply.github.com>
2025-01-28 17:00:13 -0600

    se realizo el ejercicio 2 completo

--- Condicionales/src/ParImar.java
+++ Condicionales/src/ParImar.java
@@ -1,8 +1,19 @@
+
+import java.util.Scanner;
 public class ParImar {
 
     public static void main(String[] args) {
 
+        Scanner numero = new Scanner(System.in);
+
         System.out.println("Ingrese un numero entero: ");
-        Int num =
+        int num = numero.nextInt();
+
+        if(num%2==0){
+            System.out.println("Par");
+        }
+        else{
+            System.out.println("Impar");
+        }
     }
 }
