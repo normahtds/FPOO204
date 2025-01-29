7ecad55b2e65c950c5471b0bf6d362381b6fa6df
normahtds <125906525+normahtds@users.noreply.github.com>
2025-01-29 15:37:21 -0600

    te termino por completo el ejercicio 3

--- Actividad5/src/TabMultiplicar.java
+++ Actividad5/src/TabMultiplicar.java
@@ -6,9 +6,10 @@ public class TabMultiplicar {
         System.out.println("Tabalas de multiplicar: ");
 
         for(int i=1; i<=10; i++){
-            for (int j=1; j<=i; j++){
-                System.out.print()
+            for (int j=1; j<=10; j++){
+                System.out.print(i + " x " + j + " = " + (i*j));
             }
+            System.out.println();
         }
     }
 }
