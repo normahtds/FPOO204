7342a31a4ca331f16a4a1be124dc7775d6aec514
normahtds <125906525+normahtds@users.noreply.github.com>
2025-02-04 16:48:05 -0600

    primer ejercicio realizado

--- Examen1p/src/Ejercicio1.java
+++ Examen1p/src/Ejercicio1.java
@@ -3,15 +3,12 @@ import java.util.Scanner;
 public class Ejercicio1 {
 
     public static void main(String[] args) {
-        Scanner op = new Scanner(System.in);
 
         System.out.println("Serie de numeros");
-
-        for (int i = 10; i > 1000; i++){
-            
-
-                System.out.printf( i + " ," + j + "\n");
-            }
+        int resul= 0;
+        for (int i = 10; i <= 1000; i=i+10){
+            resul= i+10;
+            System.out.printf( i + " ," + resul + "\n");
         }
     }
 }
