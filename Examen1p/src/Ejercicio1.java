9f0ced29443a8cc94a5899141de0c63abb1ee458
normahtds <125906525+normahtds@users.noreply.github.com>
2025-02-04 17:15:20 -0600

    se termino el examen

--- Examen1p/src/Ejercicio1.java
+++ Examen1p/src/Ejercicio1.java
@@ -4,7 +4,7 @@ public class Ejercicio1 {
 
     public static void main(String[] args) {
 
-        System.out.println("Serie de numeros");
+        System.out.println("Serie de numeros del 10 al 1000");
         int resul= 0;
         for (int i = 10; i <= 1000; i=i+10){
             resul= i+10;
--- Examen1p/src/Ejercicio2.java
+++ Examen1p/src/Ejercicio2.java
@@ -6,12 +6,25 @@ public class Ejercicio2 {
         System.out.println("Ingresa un numero: ");
         int num = sc.nextInt();
         int res = 0;
+        int resu = 1;
 
 
-        if (num !=0 ){
-            res= (num/2);
-            System.out.println(res);
+        for (int i = 0 ; i <= res ; i++) {
+            if (num % 2 == 0) {
+                res = (num / 2);
+                System.out.println(res);
+                if (res %2 == 1) {
+                    resu= (res / 2);
+                    System.out.println(resu);
 
+                }
+            }
+            else {
+                res = (num * 3 + 1);
+                System.out.println(res);
+                if (res %2 == 0)
+                    resu= (res / 2);
+            }
 
 
         }
