e23e20bcf8b25923c4d055762924c5e85abdd55a
normahtds <125906525+normahtds@users.noreply.github.com>
2025-02-04 16:42:19 -0600

    avance de examen

--- Examen1p/src/Ejercicio1.java
+++ Examen1p/src/Ejercicio1.java
@@ -1,2 +1,17 @@
+
+import java.util.Scanner;
 public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        Scanner op = new Scanner(System.in);
+
+        System.out.println("Serie de numeros");
+
+        for (int i = 10; i > 1000; i++){
+            
+
+                System.out.printf( i + " ," + j + "\n");
+            }
+        }
+    }
 }
--- Examen1p/src/Ejercicio2.java
+++ Examen1p/src/Ejercicio2.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
 public class Ejercicio2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingresa un numero: ");
+        int num = sc.nextInt();
+        int res = 0;
+
+
+        if (num !=0 ){
+            res= (num/2);
+            System.out.println(res);
+
+
+
+        }
+    }
 }
