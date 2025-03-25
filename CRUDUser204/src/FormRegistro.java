b80b5a3afb7c2e4c5428f810c30356bb90a4863f
normahtds <125906525+normahtds@users.noreply.github.com>
2025-03-25 16:53:05 -0600

    practica 17 select completa

--- CRUDUser204/src/FormRegistro.java
+++ CRUDUser204/src/FormRegistro.java
@@ -35,7 +35,7 @@ public class FormRegistro extends javax.swing.JFrame {
         btnGuardar = new javax.swing.JButton();
         btnLimpiar = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(0, 51, 255));
--- CRUDUser204/src/UserCRUD.java
+++ CRUDUser204/src/UserCRUD.java
@@ -7,6 +7,41 @@
  *
  * @author hanar
  */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
 public class UserCRUD {
     
+    //metodo para Consultar ID
+    public ResultSet buscarPorId(int id){
+        String sqlBuscar = "SELECT * FROM usuarios WHERE id= ?";
+        
+        try{//manejo de excepciones
+            PreparedStatement ps= conexion.prepareStatement(sqlBuscar);
+            ps.setInt(1,id);
+            return ps.executeQuery();
+        }
+        catch(SQLException e){
+            System.out.println("Error al buscar por Id "+ e.getMessage());
+            return null;
+            
+        }
+    }//fin de la consulta ID
+    
+    public ResultSet obtenerTodos (){
+        
+        String sqlTodos="SELECT * FROM usuarios";
+        
+        try{
+            PreparedStatement ps= conexion.prepareStatement(sqlTodos);
+            return ps.executeQuery(); //es donde regresa la consulta de todos
+        }
+        catch(SQLException s){
+            System.out.println("Error al consultar todos"+ s.getMenssage());
+            return null;
+        }
+        
+    }//fin de obtener todos
 }
